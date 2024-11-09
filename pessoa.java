87ce0c9d4220e72361be2a27b277e91396d1822f
Yslain3 <yslaine.carvalho28@gmail.com>
2024-11-09 14:13:17 -0300

    Update pessoa.java

--- pessoa.java
+++ pessoa.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package atividade.pkg01;
-
+package atividade02;
 /**
  *
  * @author yslaine
  */
-public class pessoa {
-    abstract class Pessoa implements Atividade01.Printable {
+public class Pessoa implements IPrintable {
+
     protected String nome, cpf, endereco, telefone;
 
     public Pessoa(String nome, String cpf, String endereco, String telefone) {
@@ -20,11 +14,8 @@ public class pessoa {
         this.telefone = telefone;
     }
 
-    @Override
     public void printInfo() {
         System.out.println(getClass().getSimpleName() + " " + nome + " - Cpf: " + cpf + " - End.: " + endereco + " - Tel.: " + telefone);
     }
 }
 
-    
-}
