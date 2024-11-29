38c94343027b9fcdcaff89259883f000f9ca46f5
Yslain3 <yslaine.carvalho28@gmail.com>
2024-11-29 13:09:52 -0300

    Update Atividade01.java

--- Atividade01.java
+++ Atividade01.java
@@ -108,7 +108,10 @@ class Coordenador extends Professor {
 }
 
 // Classe Curso
-class Curso {
+package com.mycompany.atividade02;
+
+public class Curso {
+
     private String nomeCurso;
     private int quantidadeSemestres;
     private Coordenador coordenador;
@@ -117,6 +120,11 @@ class Curso {
         this.nomeCurso = nomeCurso;
         this.quantidadeSemestres = quantidadeSemestres;
         this.coordenador = coordenador;
+        
+        // Vincula o curso ao coordenador, quando o curso é criado
+        if (coordenador != null) {
+            coordenador.setCurso(this);
+        }
     }
 
     public String getNomeCurso() {
@@ -126,7 +134,17 @@ class Curso {
     public Coordenador getCoordenador() {
         return coordenador;
     }
+
+    // Método para exibir informações do curso
+    public void printInfo() {
+        System.out.println("Curso: " + nomeCurso + " - Semestres: " + quantidadeSemestres);
+        if (coordenador != null) {
+            System.out.print("Coordenador: ");
+            coordenador.printInfo(); // Exibe as informações do coordenador
+        }
+    }
 }
+
 
 // Classe Turma
 package com.mycompany.atividade02;
